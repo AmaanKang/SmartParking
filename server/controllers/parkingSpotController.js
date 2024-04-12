bd9d5f05da17c19480303571678d89b9ff87cbe6
Amandeep Kaur <amankang344@gmail.com>
2024-04-11 21:38:19 -0400

    Added basic functions to controllers

--- server/controllers/parkingSpotController.js
+++ server/controllers/parkingSpotController.js
@@ -2,11 +2,26 @@ const ParkingSpot = require('../models/parkingSpot');
 
 // Get all parking spots in a lot
 exports.getAllParkingSpots = async(req, res) => {
-
+    try{
+        const parkingSpots = await ParkingSpot.find();
+        res.json(parkingSpots);
+    } catch(err){
+        res.status(500).json({message: err.message});
+    }
 };
 
 // Add a new parking spot
 exports.addParkingSpot = async(req, res) => {
+    const parkingSpot = new ParkingSpot({
+        spotId: req.body.spotId,
+        location: req.body.location
+    });
 
+    try{
+        const newParkingSpot = await parkingSpot.save();
+        res.status(201).json(newParkingSpot);
+    } catch(err){
+        res.status(400).json({message: err.message});
+    }
 };
 
--- server/controllers/sensorController.js
+++ server/controllers/sensorController.js
@@ -2,10 +2,25 @@ const Sensor = require('../models/sensor');
 
 // Get all sensors
 exports.getAllSensors = async(req, res) => {
-    
+    try{
+        const sesors = await Sensor.find();
+        res.json(sensors);
+    } catch (err){
+        res.status(500).json({message: err.message});
+    }
 };
 
 // Add a new sensor
 exports.addSensor = async(req, res) => {
-    
+    const sensor = new Sensor({
+        sensorId: req.body.sensorId,
+        parkingSpot: req.body.parkingSpot
+    });
+
+    try{
+        const newSensor = await sensor.save();
+        res.status(201).json(newSensor);
+    } catch(err){
+        res.status(400).json({message: err.message});
+    }
 };
