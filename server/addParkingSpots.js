17d0ef2019dbb9b0e52bd91c5ca4865929183951
Amandeep Kaur <amankang344@gmail.com>
2024-04-15 22:46:04 -0400

    Adding data to server

--- server/addParkingSpots.js
+++ server/addParkingSpots.js
@@ -1,56 +1,65 @@
+require('dotenv').config();
+
 const MongoClient = require('mongodb').MongoClient;
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
-
+const url = process.env.MONGODB_URI;
 // Database Name
 const dbName = 'SmartParking';
 
 // Create a new MongoClient
 const client = new MongoClient(url);
 
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected successfully to server");
+async function run() {
+  try {
+    // Use connect method to connect to the Server
+    await client.connect();
+    console.log("Connected successfully to server");
 
-  const db = client.db(dbName);
+    const db = client.db(dbName);
 
-  // Get the parkingSpots collection
-  const collection = db.collection('ParkingSpots');
+    // Get the parkingSpots collection
+    const collection = db.collection('ParkingSpots');
+    console.log(collection);
 
-  // Insert some parkingSpots
-  const spots = [
-    { spotId: 'A1', subColumn: 'left', status: 'free'},
-        { spotId: 'A1', subColumn: 'right', status: 'free'},
-        { spotId: 'A2', subColumn: 'left', status: 'occupied' },
-        { spotId: 'A2', subColumn: 'right', status: 'free'},
-        { spotId: 'A3', subColumn: 'left', status: 'free' },
-        { spotId: 'A3', subColumn: 'right', status: 'occupied'},
-        { spotId: 'B1', subColumn: 'left', status: 'occupied'},
-        { spotId: 'B1', subColumn: 'right', status: 'free'},
-        { spotId: 'B2', subColumn: 'left', status: 'free'},
-        { spotId: 'B2', subColumn: 'right', status: 'occupied'},
-        { spotId: 'B3', subColumn: 'left', status: 'free'},
-        { spotId: 'B3', subColumn: 'right', status: 'occupied'},
-        { spotId: 'C1', subColumn: 'left', status: 'occupied'},
-        { spotId: 'C2', subColumn: 'left', status: 'free'},
-        { spotId: 'C3', subColumn: 'left', status: 'occupied'},
-        { spotId: 'C1', subColumn: 'right', status: 'free'},
-        { spotId: 'C2', subColumn: 'right', status: 'free'},
-        { spotId: 'C3', subColumn: 'right', status: 'occupied'},
-        { spotId: 'D1', subColumn: 'left', status: 'free'},
-        { spotId: 'D2', subColumn: 'left', status: 'free'},
-        { spotId: 'D3', subColumn: 'left',status: 'occupied'},
-        { spotId: 'D1', subColumn: 'right', status: 'free'},
-        { spotId: 'D2', subColumn: 'right', status: 'occupied'},
-        { spotId: 'D3', subColumn: 'right',status: 'free'},
-    // Add more parking spots here
-  ];
+    // Insert some parkingSpots
+    const spots = [
+      { spotId: 'A1', subColumn: 'left', status: 'free'},
+          { spotId: 'A1', subColumn: 'right', status: 'free'},
+          { spotId: 'A2', subColumn: 'left', status: 'occupied' },
+          { spotId: 'A2', subColumn: 'right', status: 'free'},
+          { spotId: 'A3', subColumn: 'left', status: 'free' },
+          { spotId: 'A3', subColumn: 'right', status: 'occupied'},
+          { spotId: 'B1', subColumn: 'left', status: 'occupied'},
+          { spotId: 'B1', subColumn: 'right', status: 'free'},
+          { spotId: 'B2', subColumn: 'left', status: 'free'},
+          { spotId: 'B2', subColumn: 'right', status: 'occupied'},
+          { spotId: 'B3', subColumn: 'left', status: 'free'},
+          { spotId: 'B3', subColumn: 'right', status: 'occupied'},
+          { spotId: 'C1', subColumn: 'left', status: 'occupied'},
+          { spotId: 'C2', subColumn: 'left', status: 'free'},
+          { spotId: 'C3', subColumn: 'left', status: 'occupied'},
+          { spotId: 'C1', subColumn: 'right', status: 'free'},
+          { spotId: 'C2', subColumn: 'right', status: 'free'},
+          { spotId: 'C3', subColumn: 'right', status: 'occupied'},
+          { spotId: 'D1', subColumn: 'left', status: 'free'},
+          { spotId: 'D2', subColumn: 'left', status: 'free'},
+          { spotId: 'D3', subColumn: 'left',status: 'occupied'},
+          { spotId: 'D1', subColumn: 'right', status: 'free'},
+          { spotId: 'D2', subColumn: 'right', status: 'occupied'},
+          { spotId: 'D3', subColumn: 'right',status: 'free'},
+      // Add more parking spots here
+    ];
 
-  collection.insertMany(spots, function(err, result) {
-    if (err) throw err;
-    console.log("Inserted documents into the collection");
-    client.close();
-  });
-});
+    // Insert the spots into the collection
+    const result = await collection.insertMany(spots);
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    // Ensure that the client will close when finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
