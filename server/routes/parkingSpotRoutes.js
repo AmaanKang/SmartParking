a9cbed26cfb417e4135a6c71600681f21db402f0
Amandeep Kaur <amankang344@gmail.com>
2024-04-20 21:56:55 -0400

    Sensor data

--- server/routes/parkingSpotRoutes.js
+++ server/routes/parkingSpotRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getAllParkingSpots, addParkingSpot, removeParkingSpot, updateParkingSpot } = require('../controllers/parkingSpotController');
 const isAdmin = require('./middleware');
+const parkingSpot = require('../models/parkingSpot');
 
 // Route to get all parking spots
 router.get('/', getAllParkingSpots);
@@ -11,5 +12,28 @@ router.get('/', getAllParkingSpots);
 router.post('/admin', addParkingSpot);
 router.delete('/admin', removeParkingSpot);
 router.put('/admin', updateParkingSpot);
+
+// Route to update parking spot status
+// TODO: Modify the function to get the data from sensor
+router.put('/:spotId/status', async (req, res) => {
+    try {
+      // Get sensor data from request body
+      const sensorData = req.body.sensorData;
+  
+      // Determine status based on sensor data
+      const status = sensorData.carPresent ? 'occupied' : 'free';
+  
+      // Update parking spot status in database
+      const parkingSpot = await ParkingSpot.findOneAndUpdate(
+        { spotId: req.params.spotId },
+        { status: status },
+        { new: true }
+      );
+  
+      res.json(parkingSpot);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
 
 module.exports = router;
