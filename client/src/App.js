07fea78a1d681a73424e44edab1bc20bd7a155d1
Amandeep Kaur <amankang344@gmail.com>
2024-04-25 22:26:47 -0400

    API issues in firebase

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,39 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage.js';
 import MapPage from './components/MapPage.js';
 import Login from './components/login.js';
-import firebase from 'firebase/app';
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import { getAuth, onAuthStateChanged} from "firebase/auth";
 
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 function App() {
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
-    /**firebase.auth().onAuthStateChanged(user => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
       if (user) {
         setIsAdmin(true);
       } else {
         setIsAdmin(false);
       }
-    });*/
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
   },[])
-  
+
   return (
     <Router>
       <Routes>
