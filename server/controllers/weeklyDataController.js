7a97b493b5967b8df9b0b2921aec2ebd92b69057
Amandeep Kaur <amankang344@gmail.com>
2024-05-31 21:41:52 -0400

    File cleanup

--- server/controllers/weeklyDataController.js
+++ server/controllers/weeklyDataController.js
@@ -1,5 +1,6 @@
 const WeeklyData = require('../models/weeklyData');
 
+// This will store the occupied spots in last 7 days + average of the occupied spots per hour of the day
 exports.getWeeklyData = async(req, res) => {
     try{
         const weeklyDatas = await WeeklyData.find();
